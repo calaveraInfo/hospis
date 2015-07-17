7c437298285900c6e6f33fe56e4d5770c7466dd8
calaveraInfo <frantisek.rezac@calavera.info>
2015-07-17 17:19:44 +0200

    Aktualni stav js klienta

--- rest/web/src/main/webapp/js/app.js
+++ rest/web/src/main/webapp/js/app.js
@@ -32,12 +32,11 @@ var Menu = React.createClass({
         );
     }
 });
-
+ 
 var TextInput = React.createClass({
 	getValue: function () {
 		return React.findDOMNode(this.refs.inputField).value;
 	},
-
 	render: function() {
         return (
             <div className="row">
@@ -45,57 +44,46 @@ var TextInput = React.createClass({
 					<label htmlFor={this.props.inputId}>Vstup</label>
 				</div>
 				<div className="col-sm-8 col-md-10">
-					<input type="text" id={this.props.inputId} ref="inputField" className="form-control" placeholder="text" />
+					<input type="text" id={this.props.inputId} className="form-control" placeholder="text" ref="inputField" />
 				</div>
 			</div>
         );
     }
 });
-
-
-
+ 
 var Login = React.createClass({
-	handleLogin: function(e) {
+	onLogin: function(e) {
 		e.preventDefault();
-		var user = this.refs.loginUser.getValue();//React.findDOMNode(this.refs.loginUser).value.trim();
-		var password = this.refs.loginPassword.getValue();//React.findDOMNode(this.refs.loginPassword).value;
-		if (!user || !password) {
-		  return;
+		if (this.props.onLogin) {
+			this.props.onLogin({
+				user: this.refs.loginUser.getValue(),
+				password: this.refs.loginPassword.getValue()
+			});
 		}
-
-		//React.findDOMNode(this.refs.loginUser).value = '';
-		//React.findDOMNode(this.refs.loginPassword).value = '';
-		this.props.onLoginSubmit({user: user, password: password});
 	},
-
-	observer: Rx.Observer.create(function(valueChanged) {
-		log.info(valueChanged);
-	}, function() {}, function() {}),
-
     render: function() {
         return (
-            <form className="login" onSubmit={this.handleLogin}>
-            	<TextInput ref="loginUser" inputId="loginUser" />
-            	<TextInput ref="loginPassword" inputId="loginPassword" />
+            <form className="login" onSubmit={this.onLogin}>
+            	<TextInput inputId="loginUser" ref="loginUser"/>
+            	<TextInput inputId="loginPassword" ref="loginPassword" />
 				<button type="submit" className="btn btn-primary">Login</button>
 			</form>
         );
     }
 });
-
-var menuItems = [ { text: 'Login', href: '#' } ];
-
+ 
 var App = React.createClass({
 	getInitialState: function() {
 		return {
-			menuItems: menuItems,
+			menuItems: [ { text: 'Login', href: '#' } ],
 			loginData: {
 				user: "jnovak",
 				password: "he5l0"
-			}};
+			}
+		};
 	},
 	handleLogin: function(loginData) {
-		this.setState({ menuItems: [ { text: loginData.user, href: '#' } ] });
+		this.setState({ menuItems: [ { text: loginData.user, href: '#' } ], loginData: loginData });
 	},
     render: function() {
         return (
@@ -103,13 +91,13 @@ var App = React.createClass({
 				<Menu menuItems={this.state.menuItems} />
 				<div className="container">
 					<h1>Hospis login</h1>
-					<Login onLoginSubmit={this.handleLogin} />
+					<Login onLogin={this.handleLogin}/>
 				</div>
 			</div>
         );
     }
 });
-
+ 
 React.render(
     <App />,
     document.body
