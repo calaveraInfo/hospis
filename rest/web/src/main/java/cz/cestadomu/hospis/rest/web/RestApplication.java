4db10d8afa02e26ae5e778cbda776034269bd009
Frantisek Rezac <frantisek.rezac@profinit.eu>
2015-07-26 20:50:30 +0200

    HOSPIS-11 Aktualni stav implementace

--- rest/web/src/main/java/cz/cestadomu/hospis/rest/web/RestApplication.java
+++ rest/web/src/main/java/cz/cestadomu/hospis/rest/web/RestApplication.java
@@ -1,6 +1,10 @@
 package cz.cestadomu.hospis.rest.web;
 
-import java.util.Collection;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,17 +14,13 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import cz.cestadomu.hospis.model.Credentials;
-import cz.cestadomu.hospis.rest.web.gateway.AuthenticationGateway;
+import cz.cestadomu.hospis.rest.web.authentication.HospisAuthenticationProvider;
 
 @SpringBootApplication
 @ImportResource("classpath:integration.xml")
@@ -34,78 +34,29 @@ public class RestApplication extends SpringBootServletInitializer {
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		private SecurityProperties security;
+		public class HospisAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+			@Override
+			public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication)
+					throws IOException, ServletException {
+				response.setStatus(HttpServletResponse.SC_OK);
+				response.setContentType("application/json");
+				response.getWriter().print("{ \"user\": \"" + authentication.getName() + "\" }");
+			}
+		}
 
 		@Autowired
-		private AuthenticationGateway authenticationGateway;
+		private HospisAuthenticationProvider hospisAuthenticationProvider;
 
 		@Override
 		protected void configure(final HttpSecurity http) throws Exception {
-			http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+			http.csrf().disable().authorizeRequests().regexMatchers("/private/.*").authenticated().and().formLogin().loginPage("/login.html")
+					.successHandler(new HospisAuthenticationSuccessHandler()).usernameParameter("user").passwordParameter("password").and().logout()
+					.logoutUrl("/logout.html");
 		}
 
 		@Override
 		public void configure(final AuthenticationManagerBuilder auth) throws Exception {
-			auth.authenticationProvider(new AuthenticationProvider() {
-
-				@Override
-				public boolean supports(@SuppressWarnings("rawtypes") final Class authentication) {
-					if (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication)) {
-						return true;
-					} else {
-						return false;
-					}
-				}
-
-				@Override
-				public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
-					final Credentials credentials = new Credentials();
-					credentials.setUsername(authentication.getPrincipal().toString());
-					credentials.setPassword(authentication.getCredentials().toString());
-					if (ApplicationSecurity.this.authenticationGateway.authenticate(credentials)) {
-						return new Authentication() {
-
-							@Override
-							public String getName() {
-								return authentication.getName();
-							}
-
-							@Override
-							public void setAuthenticated(final boolean isAuthenticated) throws IllegalArgumentException {
-
-							}
-
-							@Override
-							public boolean isAuthenticated() {
-								return true;
-							}
-
-							@Override
-							public Object getPrincipal() {
-								return authentication.getPrincipal();
-							}
-
-							@Override
-							public Object getDetails() {
-								return authentication.getDetails();
-							}
-
-							@Override
-							public Object getCredentials() {
-								return authentication.getCredentials();
-							}
-
-							@Override
-							public Collection<? extends GrantedAuthority> getAuthorities() {
-								return authentication.getAuthorities();
-							}
-						};
-					} else {
-						return null;
-					}
-				}
-			});
+			auth.authenticationProvider(this.hospisAuthenticationProvider);
 		}
 	}
 }
--- rest/web/src/main/webapp/js/app.js
+++ rest/web/src/main/webapp/js/app.js
@@ -1,14 +1,19 @@
 var Menu = React.createClass({
+	getInitialState: function() {
+		return {
+			menuItems: [ { text: 'Login', href: '#' } ]
+		};
+	},
 	render: function() {
-		var menuItems = this.props.menuItems.map(function (menuItem) {
+		var menuItems = this.state.menuItems.map(function (menuItem) {
 			return (
 				<a href={menuItem.href} key={menuItem.text}>
 					{menuItem.text}
 				</a>
 			);
 		});
-        return (
-            <nav className="navbar navbar-inverse navbar-fixed-top">
+		return (
+			<nav className="navbar navbar-inverse navbar-fixed-top">
 				<div className="container">
 					<div className="navbar-header">
 						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
@@ -29,76 +34,81 @@ var Menu = React.createClass({
 					</div>
 				</div>
 			</nav>
-        );
-    }
+		);
+	}
 });
  
 var TextInput = React.createClass({
-	getValue: function () {
-		return React.findDOMNode(this.refs.inputField).value;
-	},
 	render: function() {
-        return (
-            <div className="row">
+		return (
+			<div className="row">
 				<div className="col-sm-4 col-md-2">
 					<label htmlFor={this.props.inputId}>Vstup</label>
 				</div>
 				<div className="col-sm-8 col-md-10">
-					<input type="text" id={this.props.inputId} className="form-control" placeholder="text" ref="inputField" />
+					<input type="text" id={this.props.inputId} className="form-control" placeholder="text"
+						onChange={e => {this.props.inputStream.onNext({id: this.props.inputId, value: e.target.value});}} />
 				</div>
 			</div>
-        );
-    }
+		);
+	}
 });
  
 var Login = React.createClass({
-	onLogin: function(e) {
-		e.preventDefault();
-		if (this.props.onLogin) {
-			this.props.onLogin({
-				user: this.refs.loginUser.getValue(),
-				password: this.refs.loginPassword.getValue()
-			});
-		}
+	inputStream: new Rx.Subject(),
+	submissionStream: new Rx.Subject(),
+	componentWillMount: function () {
+		this.submissionStream
+			.withLatestFrom(
+				this.inputStream
+					.scan(new (Immutable.Record({user: '', password: ''}))(), (form, input) => form.set(input.id, input.value)),
+				(clickEvent, formValue) => formValue)
+			.flatMap(credentials => Rx.Observable.fromPromise(jQuery.post(window.location.href, credentials.toObject())))
+			.filter(authentication => authentication.user ? true : false)
+			.map(authentication => authentication.user)
+			.subscribe(this.props.loginStream);
 	},
-    render: function() {
-        return (
-            <form className="login" onSubmit={this.onLogin}>
-            	<TextInput inputId="loginUser" ref="loginUser"/>
-            	<TextInput inputId="loginPassword" ref="loginPassword" />
+	render: function() {
+		return (
+			<form className="login" onSubmit={e => {e.preventDefault(); this.submissionStream.onNext(e);} }>
+				<TextInput inputId="user" ref="loginUser" inputStream={this.inputStream}/>
+				<TextInput inputId="password" ref="loginPassword" inputStream={this.inputStream} />
 				<button type="submit" className="btn btn-primary">Login</button>
 			</form>
-        );
-    }
+		);
+	}
 });
- 
+
+var LogedInUser = React.createClass({
+	componentWillMount: function () {
+		this.props.loggedUserStream.subscribeOnNext(loggedUser => { this.setState({user: loggedUser}); });
+	},
+	render: function() {
+		return (
+			<div>
+				{this.state.user}
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
-	getInitialState: function() {
-		return {
-			menuItems: [ { text: 'Login', href: '#' } ],
-			loginData: {
-				user: "jnovak",
-				password: "he5l0"
-			}
-		};
-	},
-	handleLogin: function(loginData) {
-		this.setState({ menuItems: [ { text: loginData.user, href: '#' } ], loginData: loginData });
-	},
-    render: function() {
-        return (
-            <div>
-				<Menu menuItems={this.state.menuItems} />
+	loginStream: new Rx.BehaviorSubject('xxx'),
+	render: function() {
+		return (
+			<div>
+				<Menu />
 				<div className="container">
+					<LogedInUser loggedUserStream={this.loginStream}/>
 					<h1>Hospis login</h1>
-					<Login onLogin={this.handleLogin}/>
+					<Login loginStream={this.loginStream} />
 				</div>
 			</div>
-        );
-    }
+		);
+	}
 });
  
 React.render(
-    <App />,
-    document.body
+	<App />,
+	document.body
 );
