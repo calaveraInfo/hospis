88df447fcd487d3e903037d50b8f8a7d87cc2b16
Frantisek Rezac <frantisek.rezac@calavera.info>
2015-11-14 23:13:51 +0100

    HOSPIS-11 Presun endpointu do properties konfigurace

--- core/lib/src/test/java/cz/cestadomu/hospis/core/lib/routes/AuthenticationRouteTest.java
+++ core/lib/src/test/java/cz/cestadomu/hospis/core/lib/routes/AuthenticationRouteTest.java
@@ -7,14 +7,17 @@ import org.apache.camel.ProducerTemplate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cz.cestadomu.hospis.core.lib.CoreLibConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CoreLibConfiguration.class)
+@TestPropertySource("classpath:/core-test.properties")
 public class AuthenticationRouteTest {
 
 	@Autowired
@@ -22,6 +25,9 @@ public class AuthenticationRouteTest {
 
 	@Autowired
 	private ApplicationContext context;
+  
+  @Value("${endpoint.authentication}")
+  private String authenticationChannel;
 
 	@Test
 	public void simpleAuthentication() throws CamelExecutionException, IOException {
