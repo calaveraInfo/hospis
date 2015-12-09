724953405b43b43d175953f82b03ebb0cc34aa96
Frantisek Rezac <frantisek.rezac@calavera.info>
2015-12-09 23:24:34 +0100

    Basic client build is part of the full build

---
+++ client/lib/frontend/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: {
+		index: "./index.js"
+	},
+	output: {
+		path: "../src/main/resources/public/generated-bundle",
+		filename: "[name].js"
+	},
+	module: {
+		loaders: [
+			{ test: /\.css$/, loader: "style!css" },
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	}
+}
