f662d9486e1399968d7d7c8163b998f6bd211adb
Frantisek Rezac <frantisek.rezac@calavera.info>
2015-12-21 21:20:52 +0100

    Refactoring of mock configuration for cleaner separation

--- core/lib/src/main/java/cz/cestadomu/hospis/core/lib/MockProfile.java
+++ core/lib/src/main/java/cz/cestadomu/hospis/core/lib/MockProfile.java
@@ -1,14 +1,12 @@
 package cz.cestadomu.hospis.core.lib;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Profile(CoreLib.MOCK_PROFILE)
-@PropertySource("classpath:/core-mock.properties")
-@ImportResource(locations = "classpath:/mock.xml")
+@ComponentScan(basePackages = "cz.cestadomu.hospis.mock")
 public class MockProfile {
 
 }
---
+++ core/mock/src/main/java/cz/cestadomu/hospis/mock/CoreMock.java
@@ -0,0 +1,9 @@
+package cz.cestadomu.hospis.mock;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/core-mock.properties")
+public class CoreMock {
+}
